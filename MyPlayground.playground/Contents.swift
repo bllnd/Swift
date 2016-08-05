6cb25084e745ef393336b851871c4f231d7240f7
bllnd <bllnd@mac.com>
2016-08-05 12:33:34 -0700

    Created Swift repo

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,46 @@
+
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//  Strings
+
+var str = "Hello, playground"
+
+var name:String = "Rob"
+
+print("Hello " + name + ".")
+
+//  Integers (Whole Numbers)
+
+var int:Int = 9
+
+int = int * 2
+
+int = int / 4
+
+var anotherInt = 7
+
+print(int + anotherInt)
+
+print("The value of int is \(int)")
+
+//  Double
+
+var number:Double = 8.4
+
+print(number * Double(int))
+
+// Boolean
+
+var isMale: Bool = true
+
+import UIKit
+
+var a:Double = 3.1
+
+var b:Int = 3
+
+var newNum = double * Double(int)
+
+print("\(a) muliplied by \(b) is \(newNum)")
