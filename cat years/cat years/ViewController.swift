cf37a17c12515a32c89784f85f21128429da9b5c
bllnd <bllnd@mac.com>
2016-08-10 15:32:21 -0700

    cat years

--- cat years/cat years/ViewController.swift
+++ cat years/cat years/ViewController.swift
@@ -10,6 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var ageTextField: UITextField!
+    
+    @IBOutlet var ageLabel: UILabel!
+    
+    @IBAction func submitButton(_ sender: AnyObject) {
+        let ageInCatYears = Int(ageTextField.text!)! * 7
+        
+        ageLabel.text = String(ageInCatYears)
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
