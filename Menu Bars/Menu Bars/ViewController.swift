3a4a351c87519d89e70e0dc2d062797a42a37b75
bllnd <bllnd@mac.com>
2016-08-15 16:44:58 -0700

    Navbar demo created

---
+++ Menu Bars/Menu Bars/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Menu Bars
+//
+//  Created by Abdul Wahab on 8/15/16.
+//  Copyright © 2016 Nas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func cameraPress(_ sender: AnyObject) {
+        
+        print("Camera Pressed")
+        
+    }
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
