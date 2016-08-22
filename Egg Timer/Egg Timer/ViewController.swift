f6de22376cd5ea213c7aefa5309dc35a5630f99c
bllnd <bllnd@mac.com>
2016-08-22 14:52:19 -0700

    Egg Timer App project done

--- Egg Timer/Egg Timer/ViewController.swift
+++ Egg Timer/Egg Timer/ViewController.swift
@@ -9,10 +9,72 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var timer = Timer()
+    
+    var time = 210
+    
+    func decreaseTimer() {
+    
+        if time > 0 {
+        
+        time -= 1
+        
+        eggTime.text = String(time)
+            
+        } else {
+        
+            timer.invalidate()
+            
+        }
+        
+    }
 
+    @IBOutlet var eggTime: UILabel!
+    
+    @IBAction func play(_ sender: AnyObject) {
+        
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.decreaseTimer), userInfo: nil, repeats: true)
+        
+    }
+    
+    @IBAction func pause(_ sender: AnyObject) {
+        
+        timer.invalidate()
+        
+    }
+    
+    @IBAction func plusTen(_ sender: AnyObject) {
+        
+        time += 10
+        
+        eggTime.text = String(time)
+        
+    }
+    
+    @IBAction func minusTen(_ sender: AnyObject) {
+        
+        if time > 10 {
+        
+            time -= 10
+        
+            eggTime.text = String(time)
+            
+        }
+    }
+        
+    @IBAction func resetTimer(_ sender: AnyObject) {
+        
+        time = 210
+        
+        eggTime.text = String(time)
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
