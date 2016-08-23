bae13cd6bc45784d1c59699cf09f806b0afd2d8c
bllnd <bllnd@mac.com>
2016-08-23 09:31:07 -0700

    Finished Times Table App

--- Times Table/Times Table/ViewController.swift
+++ Times Table/Times Table/ViewController.swift
@@ -8,8 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet var slider: UISlider!
+    
+    @IBOutlet var table: UITableView!
+    
+    @IBAction func sliderChanged(_ sender: AnyObject) {
+        
+        table.reloadData()
+        
+    }
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    
+    return 50
+        
+    }
+    
+
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+        
+        cell.textLabel?.text = String((Int(slider.value * 20)) * (indexPath.row + 1))
+        
+        return cell
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
