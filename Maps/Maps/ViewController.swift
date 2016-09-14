372162544712eaa12ee604141846c12a78e1bf16
bllnd <bllnd@mac.com>
2016-09-13 22:17:35 -0700

    longpress annotation in maps

--- Maps/Maps/ViewController.swift
+++ Maps/Maps/ViewController.swift
@@ -26,11 +26,46 @@ class ViewController: UIViewController, MKMapViewDelegate {
         
         let span: MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: lonDelta)
         
-        let location: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        let coordinate: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
         
-        let region: MKCoordinateRegion = MKCoordinateRegion(center: location, span: span)
+        let region: MKCoordinateRegion = MKCoordinateRegion(center: coordinate, span: span)
         
         map.setRegion(region, animated: true)
+        
+        let annotation = MKPointAnnotation()
+        
+        annotation.title = "Home"
+        
+        annotation.subtitle = "I live Here"
+        
+        annotation.coordinate = coordinate
+        
+        map.addAnnotation(annotation)
+        
+        let uilpgr = UILongPressGestureRecognizer(target: self, action: #selector(ViewController.longpress(gestureRecognizer:)))
+        
+        uilpgr.minimumPressDuration = 2
+        
+        map.addGestureRecognizer(uilpgr)
+        
+    }
+    
+    func longpress(gestureRecognizer: UIGestureRecognizer) {
+    
+        let touchPoint = gestureRecognizer.location(in: self.map)
+        
+        let coordinate = map.convert(touchPoint, toCoordinateFrom: self.map)
+        
+        let annotation = MKPointAnnotation()
+        
+        annotation.coordinate = coordinate
+        
+        annotation.title = "New Place"
+        
+        annotation.subtitle = "Maybe I'll go here someday"
+        
+        map.addAnnotation(annotation)
+        
     }
 
     override func didReceiveMemoryWarning() {
